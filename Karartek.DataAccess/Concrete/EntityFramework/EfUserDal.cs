112011abe071d19104b63d2884d7d4f5ebf8a224
berkay-demir <cmpe.berkaydemir@gmail.com>
2022-12-13 11:19:25 +0300

    List<User>

--- Karartek.DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ Karartek.DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -51,11 +51,11 @@ namespace Karartek.DataAccess.Concrete.EntityFramework
             }
         }
 
-        public User GetUserById(int id)
+        public List<User> GetUserById(int id)
         {
             
             using var context = new AppDbContext();
-            var user = context.Users.Where(x => x.Id == id).Include(x => x.UserType).Include(c => c.City).Include(d => d.District).Include(l=>l.Lawyer).Include(s => s.Student).SingleOrDefault();
+            var user = context.Users.Where(x => x.Id == id).Include(x => x.UserType).Include(c => c.City).Include(d => d.District).Include(l=>l.Lawyer).Include(s => s.Student).ToList();
             return user;
         }
 
